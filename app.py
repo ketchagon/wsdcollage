003dc308326066fa996a3c52ad468c93f5904cb2
ketchagon <woanyuan1@gmail.com>
2025-05-28 11:08:25 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,66 @@
-from flask import Flask,request,jsonify,abort
+from flask import Flask, request, jsonify, abort
 from flask_cors import CORS
 import nanoid
 import cv2
 app = Flask(__name__)
 CORS(app)
+ 
 
-@app.route('/',methods=['GET','PUT'])
+@app.route('/', methods=['GET', 'POST'])
 def root():
-    return '<h1>hello world</h1>'
+    return '<h1>hello, world</h1>'
 
 @app.route('/greet/<name>')
 def greet(name):
-    return f'hello,{name}!'
+    return f'hello, {name}!'
 
-@app.route('/v1/photos',methods=['POST'])
+@app.route('/v1/photos', methods=['POST'])
 def post_photos():
-    file = request.files['file']
-    if file.content_type == 'image/jpeg':
-        filename = f'{nanoid.generate(size=4)}.jpg'
-        file.save(f'static/{filename}')
-        img0 = cv2.imread(f'static/{filename}',cv2.IMREAD_GRAYSCALE)
-        img1 = cv2.Canny(img0,100,200)
-        cv2.imwrite(f'static/{filename}',img1)
-
-        resp = {'url':f'/static/{filename}'}
-        return jsonify(resp)
-    else:
+    if 'file' not in request.files:
         abort(400)
+    
+    file = request.files['file']
+    if file.content_type != 'image/jpeg':
+        abort(400)
+        
+    filename = f'{nanoid.generate(size=4)}.jpg'
+    file.save(f'static/{filename}')
+    img0 = cv2.imread(f'static/{filename}',1)
+    cv2.imwrite(f'static/{filename}', img0)
+    
+    resp = {'url': f'/static/{filename}'}
+    
+    # 2つ目の画像の処理
+    if 'preset_file' in request.files:
+        preset_file = request.files['preset_file']
+        if preset_file.content_type == 'image/jpeg':
+            preset_filename = f'preset_{nanoid.generate(size=4)}.jpg'
+            preset_file.save(f'static/{preset_filename}')
+            preset_img0 = cv2.imread(f'static/{preset_filename}', 1)
+            cv2.imwrite(f'static/{preset_filename}', preset_img0)
+            
+
+    cv2.resize(preset_img0,(538,413))
+
+    x = 0
+    y = 0
+
+    img_dst = img0
+
+    while y > 538:
+        x=0
+        while x > 413:
+            a = preset_img0[x][y][0]
+            b = preset_img0[x][y][1]
+            c = preset_img0[x][y][2]
+            img_dst[x+203][y+52] = [a][b][c]
+            x+=1
+        y+=1
+
+    cv2.imread('static/img_collage.jpg',img_dst)
+    resp = {'url': '/static/img_collage.jpg'}
+
+    return jsonify(resp)
 
 if __name__ == '__main__':
-    app.run(debug = True,host = '0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
